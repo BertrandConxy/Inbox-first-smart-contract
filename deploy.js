fef0b6b358f705c3db1c00fecebf57fe57af5e00
Bertrand <mutanganabertrand@gmail.com>
2022-11-07 14:48:00 +0200

    Deploy file added

--- deploy.js
+++ deploy.js
@@ -1,3 +1,25 @@
+require('dotenv').config();
+
 const HdWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { abi, bytecode } = require('./bin/contracts/Inbox');
+
+const provider = new HdWalletProvider(
+    process.env.MNEUMONIC,
+    process.env.INFURA_GOERLI,
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
